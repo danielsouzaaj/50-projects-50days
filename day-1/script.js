9b83241b78dae958f64450d48dd7e77fd55c3512
Daniel Souza <danielsouzaaj@hotmal.com>
2023-02-16 03:04:05 -0300

    adicionando função para diminuir imagem quando outra for aberta

--- day-1/script.js
+++ day-1/script.js
@@ -2,6 +2,11 @@ const images = document.querySelectorAll('conteiner-image')
 
 images.forEach(image => {
     image.addEventListener('click', (e) => {
+        removeClass()
         image.classList.add('open-image')
     })
 })
+
+function removeClass() {
+    images.forEach(image => image.classList.remove('open-image'))
+}
