31297529bb93748e0a44f6c40017528a472d4e5c
Daniel Souza <danielsouzaaj@hotmal.com>
2023-02-19 03:29:40 -0300

    chamando itens pelo DOM

---
+++ day-2/script.js
@@ -0,0 +1,4 @@
+const progress = document.querySelector('.progress')
+const btnPrev = document.querySelector('.prev')
+const btnNext = document.querySelector('.next')
+const circles = document.querySelectorAll('.circle')
