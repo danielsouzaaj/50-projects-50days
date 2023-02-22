d2ad710291eb7dc0c5d4f3eb0fe9a8cbb3c7149e
Daniel Souza <danielsouzaaj@hotmal.com>
2023-02-22 01:52:44 -0300

    adicionando eventos de click

--- day-3/script.js
+++ day-3/script.js
@@ -1,3 +1,6 @@
 const btnOpen = document.querySelector('.open')
 const btnClose = document.querySelector('.close')
 const mainConteiner = document.querySelector('.conteiner')
+
+btnOpen.addEventListener('click', () => mainConteiner.classList.add('show-nav'))
+btnClose.addEventListener('click', () => mainConteiner.classList.remove('show-nav'))
